c0a96add7ab0dac3e7e3f2c788015ca9f0ff3d53
Aidan Amato <aidanamato@comcast.net>
2021-11-13 23:36:07 -0500

    display user posts and friends on profile page

--- server/server.js
+++ server/server.js
@@ -31,11 +31,11 @@ app.use(express.json());
 // Serve static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
 
 db.once('open', () => {
   app.listen(PORT, () => {
